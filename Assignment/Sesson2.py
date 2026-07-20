6bc8d1e5b2facea8b31f12e34e838ec447f26e38
jaydeepumaraniya064-create <jaydeepumaraniya064@gmail.com>
2026-07-20 15:29:03 +0530

    session2.py

---
+++ Assignment/Sesson2.py
@@ -0,0 +1,59 @@
+# Creating variables
+
+user_name = "Hari"
+fav_app = "Instagram"
+daily_usage_hours = 3
+
+# Displaying the values
+print("User Name:", user_name)
+print("Favorite App:", fav_app)
+print("Daily Usage Hours:", daily_usage_hours)
+
+
+
+
+product_name = "Wireless Mouse"
+price = 799.50
+is_available = True
+
+print("Product Name:", product_name)
+print("Data Type:", type(product_name))
+
+print("Price:", price)
+print("Data Type:", type(price))
+
+print("Available:", is_available)
+print("Data Type:", type(is_available))
+
+
+
+# Single-line comment:
+# This script explains how Spotify recommends playlists.
+
+"""
+Multi-line comment:
+Spotify recommends playlists by analyzing:
+1. Your listening history.
+2. Songs you like or save.
+3. Artists and genres you listen to.
+4. Similar users' preferences.
+"""
+
+print("Spotify Playlist Recommendation System")
+
+
+
+order_total = 650
+delivery_region = "Ahmedabad"
+discount_percent = 15
+
+print(f"Order from {delivery_region} totals ₹{order_total} with {discount_percent}% discount.")
+
+
+
+
+if True:
+    print("Welcome to Python")
+    print("Learning Indentation")
+
+print("Program executed successfully.")
